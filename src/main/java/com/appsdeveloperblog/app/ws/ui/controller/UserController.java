02370ef9d00d51edea7c39051ad1b80eed575b2a
ravikant93 <ravikant.kant93@gmail.com>
2026-05-03 19:31:11 +0530

    sec 3.20

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,24 +1,31 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
-	@GetMapping
-	public String getUser()
+	@GetMapping(path="/{userId}")
+	public String getUser(@PathVariable String userId)
 	{
-		return "get user was called";
+		return "get user was called with userId:"+userId;
+	}
+	
+	@GetMapping
+	public String getUser(@RequestParam(value="page")int page, @RequestParam(value="limit") int limit)
+	{
+		return "get user was called with page:"+page+"and limit:"+ limit;
 	}
 	@PostMapping
 	public String CreateUser()
 	{
-		return "create user was called";
-		
+		return "create user was called";	
 	}
 	@PutMapping
 	public String UpdateUser()
